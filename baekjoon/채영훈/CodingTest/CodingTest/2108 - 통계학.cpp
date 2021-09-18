222b8578887084bab55a3675b2443460bb51be3e
kkulbeol <chaeyh04@gmail.com>
2021-09-18 20:50:15 +0900

    UPDATE * 2108

--- baekjoon/채영훈/CodingTest/CodingTest/2108 - 통계학.cpp
+++ baekjoon/채영훈/CodingTest/CodingTest/2108 - 통계학.cpp
@@ -23,7 +23,7 @@ int main()
 		cin >> v[i];
 		sum += v[i];
 	}
-	
+
 	cout << round(sum / N) << "\n";
 
 	//////////////////////////////////////////////////////////////////////////////////////////////
@@ -35,22 +35,35 @@ int main()
 
 	map<int, int> m;
 	for_each(v.begin(), v.end(), [&m](int val) { ++m[val]; });
-	vector<pair<int, int>> v_pair;
-	for_each(m.begin(), m.end(), [&v_pair](auto& p) { v_pair.push_back({ p.first,p.second }); });
-	stable_sort(v_pair.begin(), v_pair.end(), [](auto& a, auto& b) { return a.second > b.second; });
-
-	if (m.size() > 1) cout << v_pair[1].first << "\n";
-	else cout << v_pair[0].first << "\n";
+	vector<pair<int, int>> vp;
+	for_each(m.begin(), m.end(), [&vp](auto& val) { vp.push_back({ val.first,val.second }); });
+	stable_sort(vp.begin(), vp.end(), [](auto& a, auto& b) 
+		{ 
+			return a.second > b.second;
+		});
+	int max_val = -4001;
+	for (auto& [key, val] : vp)
+	{
+		if (max_val < val)
+		{
+			vp.clear();
+			max_val = val;
+		}
+		if (max_val == val) vp.push_back({ key,val });
+	}
+	if (vp.size() > 1) cout << vp[1].first << "\n";
+	else cout << vp[0].first << "\n";
 
 	//////////////////////////////////////////////////////////////////////////////////////////////
 
-	int min_val = 4001, max_val = -4001;
+	int min_val = 4001;
+	max_val = -4001;
 	for_each(v.begin(), v.end(), [&min_val, &max_val](int val)
 		{
 			if (min_val > val) min_val = val;
 			if (max_val < val) max_val = val;
 		});
-	
+
 	cout << max_val - min_val << "\n";
 
 	return 0;

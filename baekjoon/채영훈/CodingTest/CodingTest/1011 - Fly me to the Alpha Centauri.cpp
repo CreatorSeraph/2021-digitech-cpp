5a9942a13ffdd829cae5e8224f8dcc627943df77
kkulbeol <chaeyh04@gmail.com>
2021-10-05 01:52:02 +0900

    INSERT * 1011

--- baekjoon/채영훈/CodingTest/CodingTest/1011 - Fly me to the Alpha Centauri.cpp
+++ baekjoon/채영훈/CodingTest/CodingTest/1011 - Fly me to the Alpha Centauri.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -9,6 +10,22 @@ constexpr auto sp = ' ';
 int main()
 {
 	async_with_stdio;
+
+	int T, x, y;
+	cin >> T;
+
+	for (size_t i = 0; i < T; i++)
+	{
+		cin >> x >> y;
+		int dis = y - x;
+		int center = floor(sqrt(dis));
+		int move = center * 2 - 1;
+		int sub = dis - center * center;
+		if (sub != 0)
+			if (sub <= center) move++;
+			else move += 2;
+		cout << move << nl;
+	}
 
 	return 0;
 }
--- baekjoon/채영훈/CodingTest/CodingTest/1654 - 랜선 자르기.cpp
+++ baekjoon/채영훈/CodingTest/CodingTest/1654 - 랜선 자르기.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <climits>
 
 using namespace std;
 
@@ -6,11 +8,51 @@ using namespace std;
 constexpr auto nl = '\n';
 constexpr auto sp = ' ';
 
+bool check(const vector<int>& v, int mid, int need)
+{
+	int count = 0;
+	for (auto&& iter : v)
+	{
+		count += iter / mid;
+	}
+	return count >= need;
+}
+
 int main()
 {
 	async_with_stdio;
 
+	int K, N;
+	long long start = 0, end = 0, result = 0;
+	cin >> K >> N;
 
+	vector<int> v(K);
+	for (size_t i = 0; i < K; i++)
+	{
+		cin >> v[i];
+		if (end < v[i]) end = v[i];
+	}
+
+	if (check(v, end, N))
+	{
+		cout << end << nl;
+		return 0;
+	}
+
+	while (start < end)
+	{
+		long long mid = (start + end) / 2;
+
+		if (check(v, mid, N))
+		{
+			start = mid + 1;
+			result = mid;
+		}
+		else
+			end = mid - 1;
+	}
+
+	cout << result << nl;
 
 	return 0;
 }

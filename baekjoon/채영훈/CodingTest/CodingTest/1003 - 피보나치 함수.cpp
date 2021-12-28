9c02ea528b9332328b384769ae7704a09f887e17
yhoney <chaeyh04@gmail.com>
2021-12-28 19:12:01 +0900

    UPDATE * 1003

--- baekjoon/채영훈/CodingTest/CodingTest/1003 - 피보나치 함수.cpp
+++ baekjoon/채영훈/CodingTest/CodingTest/1003 - 피보나치 함수.cpp
@@ -1,16 +1,44 @@
 #include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <unordered_map>
 
 using namespace std;
 
 #define async_with_stdio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 constexpr auto nl = '\n';
 constexpr auto sp = ' ';
+constexpr auto tab = '\t';
 
 int main()
 {
-	async_with_stdio;
+	int T,N;
+	cin >> T;
 
+	vector<pair<int, int>> dp(41, pair<int, int>(0, 0));
 
+	for (size_t i = 0; i < T; i++)
+	{
+		cin >> N;
+
+		dp[0].first = 0;
+		dp[0].second = 1;
+		dp[1].first = 1;
+		dp[1].second = 0;
+		dp[2].first = 1;
+		dp[2].second = 1;
+
+		for (size_t i = 3; i <= N; i++)
+		{
+			dp[i].first = dp[i - 1].first + dp[i - 2].first;
+			dp[i].second = dp[i - 1].second + dp[i - 2].second;
+		}
+
+		cout << dp[N].second << " " << dp[N].first << nl;
+	}
 
 	return 0;
 }

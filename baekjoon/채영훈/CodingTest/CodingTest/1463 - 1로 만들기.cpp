0c78a234bbfa40da3d3f2243aaba9588a815b73c
chae young hoon <chaeyh04@gmail.com>
2021-12-29 15:11:54 +0900

    INSERT * 1463

---
+++ baekjoon/채영훈/CodingTest/CodingTest/1463 - 1로 만들기.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+#define async_with_stdio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+constexpr auto nl = '\n';
+constexpr auto sp = ' ';
+constexpr auto tab = '\t';
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	vector<int> dp(N + 1);
+
+	return 0;
+}

f6bc150b4ea77914df86033ac4be1f041b5e8ec6
daeho0818 <daeho0818@naver.com>
2022-04-28 08:07:09 +0900

    두 문제를 풀었씁니당

---
+++ baekjoon/임대호/BaekJoon/1037 약수.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	cin.tie(nullptr);
+//	cout.tie(nullptr);
+//	ios::sync_with_stdio(false);
+//
+//	int n, min = 1000001, max = 0;
+//
+//	int temp;
+//
+//	cin >> n;
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> temp;
+//
+//		if (temp < min) min = temp;
+//		if (temp > max) max = temp;
+//	}
+//
+//	cout << max * min;
+//
+//	return 0;
+//}
---
+++ baekjoon/임대호/BaekJoon/1934 최소공배수.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+	int n, a, b, _a, _b;
+	cin >> n;
+
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+		_a = a;
+		_b = b;
+
+		while (_a != _b)
+		{
+			if (_a < _b) _a += a;
+			if (_b < _a) _b += b;
+		}
+
+		cout << _a << '\n';
+	}
+
+	return 0;
+}

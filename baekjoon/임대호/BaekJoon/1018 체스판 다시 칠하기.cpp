c5bd2ad1d2fbea091461615257e55c41096190db
daeho0818 <daeho0818@naver.com>
2022-04-29 21:14:38 +0900

    세문제를 더 푸렀따

--- baekjoon/임대호/BaekJoon/1018 체스판 다시 칠하기.cpp
+++ baekjoon/임대호/BaekJoon/1018 체스판 다시 칠하기.cpp
@@ -1,131 +1,66 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-int 체스판다시칠하기()
-{
-	cin.tie(nullptr);
-	cout.tie(nullptr);
-	ios::sync_with_stdio(false);
-
-	int n, m;
-	cin >> n >> m;
-
-	vector<vector<char>> vec(n);
-
-	for (int i = 0; i < n; i++)
-	{
-		vec[i].resize(m);
-
-		for (int j = 0; j < m; j++)
-		{
-			cin >> vec[i][j];
-		}
-	}
-
-	int count, max = 0, ind_x = 0, ind_y = 0;
-	bool once = true;
-	for (int i = 0; i <= n - 8; i++)
-	{
-		for (int j = 0; j <= m - 8; j++)
-		{
-			count = 1;
-
-			for (int k = i; k < i + 8; k++)
-			{
-				for (int l = j; l < j + 8; l++)
-				{
-					if (l > j && vec[k][l] != vec[k][l - 1])
-					{
-						count++;
-
-						if (count == 64)
-						{
-							cout << 0;
-							return 0;
-						}
-
-						if (count > max)
-						{
-							max = count;
-							ind_x = l;
-							ind_y = k;
-						}
-					}
-					else if (k > i && vec[k][l] != vec[k - 1][l])
-					{
-						if (once)
-						{
-							count++;
-
-							if (count == 64)
-							{
-								cout << 0;
-								return 0;
-							}
-
-							if (count > max)
-							{
-								max = count;
-								ind_x = l;
-								ind_y = k;
-							}
-						}
-					}
-					once = !once;
-				}
-			}
-		}
-	}
-
-	if (ind_x - 7 < 0) ind_x = 7;
-	if (ind_y - 7 < 0) ind_y = 7;
-
-	vector<vector<char>> _vec = vec;
-
-	_vec[ind_y][ind_x] = _vec[ind_y][ind_x] == 'B' ? 'W' : 'B';
-
-	int result_1 = 0, result_2 = 0;
-	for (int y = ind_y; y >= ind_y - 7; y--)
-	{
-		for (int x = ind_x; x >= ind_x - 7; x--)
-		{
-			if (x < ind_x)
-			{
-				if (vec[y][x] == vec[y][x + 1])
-				{
-					result_1++;
-
-					vec[y][x] = (vec[y][x + 1] == 'B' ? 'W' : 'B');
-				}
-
-				if (_vec[y][x] == _vec[y][x + 1])
-				{
-					result_2++;
-
-					_vec[y][x] = (_vec[y][x + 1] == 'B' ? 'W' : 'B');
-				}
-			}
-			else if (y < ind_y)
-			{
-				if (vec[y][x] == vec[y + 1][x])
-				{
-					result_1++;
-
-					vec[y][x] = (vec[y + 1][x] == 'B' ? 'W' : 'B');
-				}
-
-				if (_vec[y][x] == _vec[y + 1][x])
-				{
-					result_2++;
-
-					_vec[y][x] = (_vec[y + 1][x] == 'B' ? 'W' : 'B');
-				}
-			}
-		}
-	}
-
-	cout << (result_1 < result_2 ? result_1 : result_2);
-
-	return 0;
-}
+//#include <iostream>
+//#include <vector>
+//
+//const char solved_arr[8][8] =
+//{
+//	{ 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+//	{'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+//	{ 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+//	{'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+//	{ 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+//	{'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+//	{ 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+//	{'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'}
+//};
+//
+//using namespace std;
+//int main()
+//{
+//	cin.tie(nullptr);
+//	cout.tie(nullptr);
+//	ios::sync_with_stdio(false);
+//
+//	int n, m;
+//	cin >> n >> m;
+//
+//	vector<vector<char>> v(n);
+//	for (int i = 0; i < n; i++)
+//	{
+//		v[i].resize(m);
+//
+//		for (int j = 0; j < m; j++)
+//		{
+//			cin >> v[i][j];
+//		}
+//	}
+//
+//	int min_1 = 64, min_2 = 64, count_1 = 0, count_2 = 0;
+//
+//	for (int i = 0; i < (n - 8 + 1); i++)
+//	{
+//		for (int j = 0; j < (m - 8 + 1); j++)
+//		{
+//			for (int y = i; y < 8 + i; y++)
+//			{
+//				for (int x = j; x < 8 + j; x++)
+//				{
+//					if (v[y][x] != solved_arr[y - i][x - j])
+//						count_1++;
+//					else
+//						count_2++;
+//				}
+//			}
+//
+//			if (count_1 < min_1)
+//				min_1 = count_1;
+//			if (count_2 < min_2)
+//				min_2 = count_2;
+//
+//			count_1 = count_2 = 0;
+//		}
+//	}
+//
+//	cout << (min_1 < min_2 ? min_1 : min_2);
+//
+//	return 0;
+//}
---
+++ baekjoon/임대호/BaekJoon/13241 최소공배수.cpp
@@ -0,0 +1,24 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	cin.tie(nullptr);
+//	cout.tie(nullptr);
+//	ios::sync_with_stdio(false);
+//
+//	unsigned long long a, b;
+//	cin >> a >> b;
+//
+//	auto _a = a, _b = b;
+//	while (_a != _b)
+//	{
+//		if (_a < _b)
+//			_a += a;
+//		if (_b < _a)
+//			_b += b;
+//	}
+//
+//	cout << _a;
+//
+//	return 0;
+//}

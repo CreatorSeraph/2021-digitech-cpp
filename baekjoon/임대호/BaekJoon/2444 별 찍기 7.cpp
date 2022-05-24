14b434257c2cc397a72707314ef31f4ee12dc785
daeho0818 <daeho0818@naver.com>
2022-05-24 12:53:24 +0900

    오랜만에 문제를 풀었따

---
+++ baekjoon/임대호/BaekJoon/2444 별 찍기 7.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+
+	for (int i = -n + 1; i < n; i++)
+	{
+		int j = 0;
+		for (; j < abs(i); j++)
+		{
+			cout << ' ';
+		}
+		for (j = 0; j < ((n - 1) - abs(i)) *2 + 1; j++)
+		{
+			cout << '*';
+		}
+		cout << endl;
+	}
+
+	return 0;
+}

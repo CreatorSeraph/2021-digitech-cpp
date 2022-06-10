7ea54e809452930060f0a7c95e5343c290bed483
daeho0818 <daeho0818@naver.com>
2022-06-10 20:54:50 +0900

    한문제 풂

--- baekjoon/임대호/BaekJoon/9655 돌 게임.cpp
+++ baekjoon/임대호/BaekJoon/9655 돌 게임.cpp
@@ -1,30 +1,30 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-	cin.tie(nullptr);
-	cout.tie(nullptr);
-	ios::sync_with_stdio(false);
-
-	int n, count;
-	string result;
-
-	cin >> n;
-
-	if ((n / 3) % 2 == 0)
-		count = 0;
-	else count = 1;
-
-	n %= 3;
-
-	for (int i = 1; i <= n; i++)
-		count++;
-
-	if (count % 2 == 0)
-		result = "CY";
-	else result = "SK";
-
-	cout << result;
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	cin.tie(nullptr);
+//	cout.tie(nullptr);
+//	ios::sync_with_stdio(false);
+//
+//	int n, count;
+//	string result;
+//
+//	cin >> n;
+//
+//	if ((n / 3) % 2 == 0)
+//		count = 0;
+//	else count = 1;
+//
+//	n %= 3;
+//
+//	for (int i = 1; i <= n; i++)
+//		count++;
+//
+//	if (count % 2 == 0)
+//		result = "CY";
+//	else result = "SK";
+//
+//	cout << result;
+//
+//	return 0;
+//}

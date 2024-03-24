6da50f0cc3db069801d0b476a420cf7c7629b507
daeho0818 <daeho0818@naver.com>
2024-03-24 17:40:48 +0900

    1로 만들기 일단 방법만 아이디에이션

---
+++ baekjoon/임대호/BaekJoon/1463 1로 만들기.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+// 생각 : 2부터 N까지 돌면서 3으로 나눌 때, 2로 나눌 때, 1 빼는 것 중 얘로부터 1까지 가는 데 걸리는 수 제일 작은 거를 미리 저장해놓기.
+// 그러면 수가 커질 때마다 인덱스로 참조하면 얼마나 걸리는 지 알 수 있으니까 그거 읽어서 연산하면 되지 않을까?
+int main() {
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    return 0;
+}

554e2adfca116390427ce4d1b659401de055bd6e
daeho0818 <daeho0818@naver.com>
2024-03-24 16:29:50 +0900

    팩토리얼 0의 갯수

--- baekjoon/임대호/BaekJoon/1676 팩토리얼 0의 개수.cpp
+++ baekjoon/임대호/BaekJoon/1676 팩토리얼 0의 개수.cpp
@@ -1,51 +1,41 @@
-//#include <iostream>
-//#include <vector>
-//
-//#define ull unsigned long long
-//
-//using namespace std;
-//
-//ull Factorial(int n1) {
-//    vector<ull> v;
-//
-//    v.resize(n1);
-//
-//    if (n1 <= 1)
-//        return 1;
-//
-//    v[0] = 1;
-//    for (int i = 1; i < n1; i++) {
-//        v[i] = v[i - 1] * (i + 1);
-//    }
-//
-//    return v.back();
-//}
-//
-//int main() {
-//	cin.tie(nullptr);
-//	cout.tie(nullptr);
-//	ios::sync_with_stdio(false);
-//
-//    int n;
-//    cin >> n;
-//
-//    ull factorial = Factorial(n);
-//    cout << factorial << '\n';
-//    string str = to_string(factorial);
-//
-//    int ret = 0;
-//    while (true) {
-//        char c = str.back();
-//        str.pop_back();
-//
-//        if (c == '0') {
-//            ret++;
-//        } else {
-//            break;
-//        }
-//    }
-//
-//    cout << ret;
-//
-//	return 0;
-//}
+#include <iostream>
+#include <vector>
+
+#define ull unsigned long long
+
+using namespace std;
+
+int zeroCount = 0;
+ull Factorial_Zigun(int n1) {
+    if (n1 <= 1)
+        return 1;
+
+    int factorial = 24;
+    for (int i = 4; i < n1; i++) {
+        factorial *= i + 1;
+
+        while (factorial % 10 == 0) {
+            zeroCount++;
+            factorial /= 10;
+        }
+
+        factorial %= 1000;
+    }
+
+    return factorial;
+}
+
+int 팩토리얼0의갯수() {
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+    int n;
+
+    cin >> n;
+
+    Factorial_Zigun(n);
+    cout << zeroCount;
+
+	return 0;
+}

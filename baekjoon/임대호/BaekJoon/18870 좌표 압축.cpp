d7bcce51bbe03dc14b77b9e5e993d22f853e0b1e
daeho0818 <daeho0818@naver.com>
2024-04-27 17:49:55 +0900

    좌표 압축 날먹하기

---
+++ baekjoon/임대호/BaekJoon/18870 좌표 압축.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+struct Compare {
+    bool operator() (int a, int b) {
+        return a > b;
+    }
+};
+
+int main() {
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    priority_queue<int, vector<int>, Compare> q;
+
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+
+        q.push(v[i]);
+    }
+
+    int top;
+    map<int, int> m;
+    int value = 0;
+    while (!q.empty()) {
+        top = q.top();
+        q.pop();
+
+        if (m.find(top) != m.end())
+            continue;
+
+        m.insert(make_pair(top, value++));
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << m[v[i]];
+        if (i < n - 1)
+            cout << ' ';
+    }
+
+    return 0;
+}
